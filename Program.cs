5a1f67703eb18cbbf34fb77309d03f48c38fbc57
RobsonaCosta <robson_costa9@hotmail.com>
2025-08-28 17:23:08 -0300

    Desafio SmartPhone Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("SmartPhone Iphone");
+Smartphone iphone = new Iphone(numero: "145688", modelo: "iphone6", imei: "22222", memoria: 84);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Spotify");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("SmartPhone Nokia: ");
+Smartphone nokia = new Nokia(numero: "1234", modelo: "AX2055", imei: "111111", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("whatsapp");
+
+
+
